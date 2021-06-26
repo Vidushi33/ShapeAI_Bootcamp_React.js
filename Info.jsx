5345976930391933014def2ecff8fa4a1dbafe81
Vidushi Malik <63915911+Vidushi33@users.noreply.github.com>
2021-06-26 19:40:55 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.Js</h1>
+      <p>A basic Web Development React.Js Bootcamp.</p>
+      <p>Start Date : 27th June, 2021</p>
+    </div>
+  );
+}
+
+export default Info;
